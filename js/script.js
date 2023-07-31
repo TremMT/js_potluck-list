bf54313c5581c3049edec87de06b79b70bb1a594
TremMT <tremariet@gmail.com>
2023-07-30 17:27:16 -0700

    Updated but unforked, as of 7302023 the Fork'ed option on Github is grayed out for me.

--- js/script.js
+++ js/script.js
@@ -10,6 +10,8 @@ const guestList = document.querySelector(".guest-list");
 const guestCount = document.querySelector(".attendance");
 // alert when guest list is full (not yet visible)
 const guestFull = document.querySelector(".alert");
+const assignButton = document.querySelector(".assign");
+const assignedItems = document.querySelector(".assigned-items");
 
 addGuestButton.addEventListener("click", function () {
   const guest = guestInput.value;
@@ -34,6 +36,7 @@ const addToList = function (guest) {
 const updateGuestCount = function () {
   let guests = document.querySelectorAll(".guest-list li");
   guestCount.innerText = guests.length;
+
   if (guests.length === 8) {
     addGuestButton.classList.add("hide");
     guestInput.classList.add("hide");
@@ -41,3 +44,45 @@ const updateGuestCount = function () {
     guestFull.classList.remove("hide");
   }
 };
+
+const assignItems = function () {
+  const potluckItems = [
+    "GF 3-Cheese Tomato Tart",
+    "GF Cheat Lemon Pie",
+    "Pineapple Cucumber Salad",
+    "GF Honey Jalapeno Cornbread",
+    "Chili Mac",
+    "Over-Roasted Herb Chicken",
+    "Carnitas",
+    "Veggie Casserole",
+    "Asian Meatballs",
+    "Wild Rice Pilaf",
+    "Mushroom Galette",
+    "Snickers Apple Salad",
+    "THE Cheesecake (GF)",
+    "Parmensan Chopped Salad",
+    "Corn Chips, Guac, and Salsa",
+    "The Reg Pato Chips and Dip",
+    "GF Maple Bourbon Cupcakes",
+    "(A suprise, but fresh baked, I promise!!) Cookies..."
+  ];
+  const allGuests = document.querySelectorAll(".guest-list li");
+
+  for (let guest of allGuests) {
+    let randomPotluckIndex = Math.floor(Math.random() * potluckItems.length);
+
+    let randomPotluckItem = potluckItems[randomPotluckIndex];
+
+    let listItem = document.createElement("li");
+    listItem.innerText = `${guest.innerText} is bringing ${randomPotluckItem}!`;
+    //Note for later - You’re using guest.innerText to access the name inside the li element. If you used guest without innerText, you’d grab the actual list element instead of the text.
+    assignedItems.append(listItem);
+    potluckItems.splice(randomPotluckIndex, 1);
+  }
+};
+
+assignButton.addEventListener("click", function () {
+  assignItems();
+  assignButton.disabled = true;
+});
+// I'm just updating/ uploading this code unforked. There's something going on with my Git, the site won't let me fork any of the current projects I have. The Fork option is completely grayed out. :(
